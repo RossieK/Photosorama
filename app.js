45a1dce2652ea9a41d295df604be14c4b1b9145f
RossieK <rosi.kirova96@gmail.com>
2021-04-21 21:55:48 +0300

    Add more functionality

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
 const searchForm = document.querySelector('.search-form');
 const downloadBtn = document.querySelectorAll('download-btn');
+const more = document.querySelector('.more');
 let searchValue;
+let page = 1;
+let fetchLink;
+let currentSearch;
 
 //Event Listeners
 searchInput.addEventListener('input', updateInput);
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    currentSearch = searchValue;
     searchPhotos(searchValue);
 });
+
 gallery.addEventListener('click', (e) => {
     if (e.target.classList.contains("download-btn")) {
         downloadImage(e.target.href);
     }
 });
+
+more.addEventListener('click', loadMore);
 
 //Functions
 function updateInput(e) {
@@ -48,13 +56,15 @@ function generatePictures(data) {
 }
 
 async function curatedPhotos() {
-    const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1");
+    fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+    const data = await fetchApi(fetchLink);
     generatePictures(data);
 }
 
 async function searchPhotos(query) {
     clear();
-    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`);
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`;
+    const data = await fetchApi(fetchLink);
     generatePictures(data);
 }
 
@@ -78,6 +88,18 @@ async function downloadImage(url) {
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
+}
+
+async function loadMore() {
+    page++;
+    if (currentSearch) {
+        fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+    } else {
+        fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+    }
+
+    const data = await fetchApi(fetchLink);
+    generatePictures(data);
 }
 
 curatedPhotos();
